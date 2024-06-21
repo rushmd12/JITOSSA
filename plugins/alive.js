02905f1212d2e6a9bebae60e4c92c18e2d7602c4
rushmd12 <dilshanrashmika141rs12@gmail.com>
2024-06-21 08:01:42 +0530

    Update alive.js

--- plugins/alive.js
+++ plugins/alive.js
@@ -5,7 +5,7 @@ let handler = async(m, { conn, text, usedPrefix, command }) => {
     // Sound
     let name = m.pushName || conn.getName(m.sender)
     var vn = "./src/ALIVE.mp3"
-    let url = "https://github.com/omarcharaf1"
+    let url = "https://github.com/Rush-MD"
     let murl = "www.instagram.com/rush_1"
     let img = "https://telegra.ph/file/d31762a021e4dfd7eda52.jpg"
     let con = { key: { fromMe: false, participant: `${m.sender.split`@`[0]}@s.whatsapp.net`, ...(m.chat ? { remoteJid: '16504228206@s.whatsapp.net' } : {}) }, message: { contactMessage: { displayName: `${name}`, vcard: `BEGIN:VCARD\nVERSION:3.0\nN:;a,;;;\nFN:${name}\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}}
